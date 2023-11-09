cec8489830a7ef27b364ded9ec828db79c5c4de2
Shiam Sharif <shiam.sharif.07@gmail.com>
2023-11-09 20:41:12 +0600

    Hello

---
+++ M16/main.js
@@ -0,0 +1,5 @@
+var x = 10;
+x = 15;
+var y;
+
+console.log(++y);
--- M16/variable.js
+++ M16/variable.js
@@ -5,10 +5,12 @@ var number = 81;
 
 // string
 var full_name = "Shiam sharif";
-var first_name = shiam;
-var last_name = sharif;
+// var first_name = shiam;
+// var last_name = sharif;
 var university = "northern university bangladesh";
 
 //boolian
 var pass = true;
 var isPrime = false;
+
+var name = 'Shiam sharif';

bd24e3391abd111cceeafe651d6ad61248d41c1b
Shiam Sharif <shiam.sharif.07@gmail.com>
2023-11-12 21:13:35 +0600

    M-18 done

---
+++ M-18/index.js
@@ -0,0 +1,13 @@
+var x = [10,20,30,40,50,60,70,80,90,100,110];
+
+console.log(x[2]);
+
+x[1] = 99;
+console.log(x[1]);
+
+//find index of an element:
+var y = x.indexOf(990);
+console.log(y); 
+var y = x.indexOf(990);
+console.log(y); 
+
---
+++ M-18/pushPop.js
@@ -0,0 +1,5 @@
+var x = [10,20,30,40];
+x.unshift(55,66,88,77,99);
+console.log(x); 
+x.pop();
+console.log(x); 

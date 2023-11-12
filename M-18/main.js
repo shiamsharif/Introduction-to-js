830071e766ef79344dafa8bef9c38e573422a7ee
Shiam Sharif <shiam.sharif.07@gmail.com>
2023-11-12 21:20:08 +0600

    M-18 Done

--- M-18/main.js
+++ M-18/main.js
@@ -1,1 +1,10 @@
-console.log(78!=0);
+var x = ['apple','banana','orange'];
+
+var y = x.indexOf("banana");
+console.log(y);
+x[y] = "mango";
+x[y+1] = "orange";
+console.log(x);
+x.pop();
+x.push("Watermilon");
+console.log(x);

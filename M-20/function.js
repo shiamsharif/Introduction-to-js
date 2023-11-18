1b595a6673eb74373973924daec178d892fc806f
Shiam Sharif <shiam.sharif.07@gmail.com>
2023-11-18 18:13:00 +0600

    m-20

---
+++ M-20/function.js
@@ -0,0 +1,9 @@
+function sum(a,b){
+    return a+b;
+}
+
+var x = 10;
+var y = 23;
+
+var sun = sum(x,y);
+console.log(sun);
---
+++ M-20/main.js
@@ -0,0 +1,11 @@
+var x = [1,8,98,45,65,71,63];
+var y = x.slice(2,4);
+// console.log(x);
+// console.log(y);
+
+var array = "ShiamSharif will be a good developer. in sha Allah"
+// array = array.includes('ShiamSharif');
+// console.log(array); //true
+
+
+console.log(array[5]);

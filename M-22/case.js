6651ada53b39025aa637f6b5158ee699e77e5d44
Shiam Sharif <shiam.sharif.07@gmail.com>
2023-11-21 23:40:59 +0600

    m-22

---
+++ M-22/case.js
@@ -0,0 +1,10 @@
+const uname = 'blackPink';
+const input = 'blackPink';
+console.log(uname.toLowerCase());
+
+if(uname.toLowerCase() === input.toLowerCase()){
+    console.log("valid user");
+}
+else{
+    console.log("invalid user");
+}
---
+++ M-22/search.js
@@ -0,0 +1,8 @@
+const x = "tumi bondhu kala pakhi. ami jeno ki. boshontow kale tomai bolte pari nai";
+
+// const doesExist = x.includes('pakhi');  //search string or character.
+// console.log(x.indexOf('ami')); //number of index
+// console.log(doesExist);
+
+const sentences = x.split('')
+console.log(sentences);

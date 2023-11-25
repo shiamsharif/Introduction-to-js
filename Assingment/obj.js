a0375bbcbfa7f176fa6033cbc0f0e30d04063316
Shiam Sharif <shiam.sharif.07@gmail.com>
2023-11-25 21:12:46 +0600

    object

---
+++ Assingment/obj.js
@@ -0,0 +1,15 @@
+const  student = {
+    name: 'sakib khan',
+    id: 121,
+    address:'movie cinema',
+    isSingle : true,
+    friends: ['apu', 'Raz', 'Salman', 'shiam'],
+    car: {
+        brand: 'tesla',
+        price: 500000000,
+        mode: 2324
+    }
+
+}
+
+console.log(student)
--- Assingment/problem5.js
+++ Assingment/problem5.js
@@ -1,13 +1,16 @@
-const people1 =[
-    {name:'Devid j malan',friend:'Shiam'}
-]
+const people1 = {
+    name:'Devid j malan',
+    friend:'Shiam'
+};
 
-const people2 =[
-    {name:'Shiam',friend:'Sharif'}
-]
+const people2 = {
+    name:'Shiam',
+    friend:'Sharif'
+};
+
 
 function isBestFriend(people1, people2){
-    if((people1.name === people2.friend) && (people2.name === people1.friend)){
+    if((people1.name === people2.friend) && (people2['name'] === people1['friend'])){
         console.log("True");
     }
     else{
